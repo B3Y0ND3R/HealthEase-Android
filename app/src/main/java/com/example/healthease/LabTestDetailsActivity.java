47eb4aaac306cb6f4ef8d0f7c47d1f537a712a9f
sakib <sakiburrahman11.msr@gmail.com>
2024-04-14 12:44:15 +0600

    Design Lab test XML page

---
+++ app/src/main/java/com/example/healthease/LabTestDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.healthease;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LabTestDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab_test_details);
+
+        
+    }
+}
